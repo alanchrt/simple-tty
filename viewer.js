056c8705d9590c9072a77d69bc99b7fa522366d0
Alan Christopher Thomas <alan@alanct.com>
2013-11-19 17:22:26 +0000

    add basic tmux viewer

---
+++ viewer.js
@@ -0,0 +1,39 @@
+var express = require('express'),
+    http = require('http'),
+    pty = require('pty.js'),
+    term = require('term.js'),
+    path = require('path');
+
+var app = express();
+
+app.configure(function() {
+    app.use(app.router);
+    app.use(term.middleware());
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+var server = http.createServer(app).listen(process.env.VIEWER_PORT, process.env.VIEWER_BIND_IP, function() {
+    console.log("Express server listening on port " + process.env.VIEWER_PORT);
+});
+
+io = require('socket.io').listen(server);
+
+io.set('transports', ['htmlfile', 'xhr-polling', 'jsonp-polling']);
+
+io.sockets.on('connection', function(socket) {
+    var term = pty.spawn('tmux', ['-2', 'attach-session', '-t', 'simple-tty'], {
+        name: 'xterm-color',
+        cols: 160,
+        rows: 44,
+        cwd: process.env.HOME,
+        env: process.env
+    });
+
+    term.on('data', function(data) {
+        socket.emit('data', data);
+    });
+
+    socket.on('disconnect', function() {
+        term.destroy();
+    });
+});
