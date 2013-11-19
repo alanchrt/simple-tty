ca4c1cca0756c19061ef81b24061ff22d312cc25
Alan Christopher Thomas <alan@alanct.com>
2013-11-19 16:57:22 +0000

    better sizing and positioning

--- app.js
+++ app.js
@@ -21,20 +21,20 @@ io = require('socket.io').listen(server);
 io.set('transports', ['htmlfile', 'xhr-polling', 'jsonp-polling']);
 
 io.sockets.on('connection', function(socket) {
-    socket.on('data', function(data) {
-       term.write(data);
-    });
-
     var term = pty.spawn('bash', [], {
         name: 'xterm-color',
         cols: 160,
-        rows: 40,
+        rows: 44,
         cwd: process.env.HOME,
         env: process.env
     });
 
     term.on('data', function(data) {
         socket.emit('data', data);
+    });
+
+    socket.on('data', function(data) {
+       term.write(data);
     });
 
     socket.on('disconnect', function() {
